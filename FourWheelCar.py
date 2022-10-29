9e1a0a4acd40683adde18c3b780e35ce3d114271
Hayato Kato <hayatokato8810@gmail.com>
2022-10-29 15:21:29 -0700

    Track visualizer

--- FourWheelCar.py
+++ FourWheelCar.py
@@ -10,24 +10,51 @@ dir = os.path.dirname(os.path.realpath(__file__))
 sys.path.append(dir)
 
 import argparse
-import pandas as pd
+import matplotlib.pyplot as plt
 import numpy as np
+import pandas as pd
 
 class FourWheelCar():
-	def __init__(self):
+	def __init__(self, trackData):
 		print("Initializing")
+		self.v = 0			# speed
+		self.ax = 0			# longitudinal acceleration
+		self.ay = 0			# lateral acceleration
+		self.dn = 0			# normal distance to centerline
+		self.alpha = 0	# angle relative to centerline
+		self.slip = 0		# slip angle
+		self.yaw = 0		# yaw rate
+
+		self.track = trackData
+		self.closestTrack = self.track[0]
+
+		self.fig, self.axs = plt.subplots(1, 1, figsize=(9, 9))
 
 	def plot(self):
 		print("Plotting")
+		self.plotTrack()
+		plt.scatter(self.closestTrack[0],self.closestTrack[1],50,
+			c='r',edgecolor='k', zorder=3)
+		self.axs.set_xlim([-1,6])
+		self.axs.set_ylim([-1,6])
+		plt.grid(True)
+		plt.show()
 
+	def plotTrack(self):
+		[tx,ty] = zip(*self.track)
+		tx = tx + (self.track[0][0],)
+		ty = ty + (self.track[0][1],)
+		plt.plot(tx,ty,'-',color='grey',lw = 72,solid_capstyle='round', zorder=1)
+		plt.plot(tx,ty,'--',color='white',lw = 2,solid_capstyle='round', zorder=2)
 
 def main(args):
 	print("Starting Program...")
 
-	df = pd.read_csv(args.input, delimiter=',')
+	df = pd.read_csv('track data/' + args.input, delimiter=',')
 	data = [tuple(row) for row in df.values]
 
-	print(data)
+	car = FourWheelCar(data)
+	car.plot()
 
 	print('Finishing Program...')
 
--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # MAE271D-Project
+
+## How to Run
+
+Type the following:
+
+python3 FourWheelCar.py -i track_data_name_here.csv
