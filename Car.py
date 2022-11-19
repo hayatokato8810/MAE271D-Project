ac30744cbb96243a2f5ac5fd6244bb1aa615b277
Hayato Kato <hayatokato8810@gmail.com>
2022-11-18 19:20:16 -0800

    Car Position Animation

--- Car.py
+++ Car.py
@@ -1,13 +1,14 @@
 import math
 import numpy as np
 import matplotlib.pyplot as plt
+import matplotlib.animation as animation
 
 class Car():
 	'''
-	y:	 lateral displacement
-	v:	 lateral velocity
+	y:   lateral displacement
+	v:   lateral velocity
 	psi: heading angle
-	r:	 angular velocity
+	r:   angular velocity
 	'''
 	def __init__(self, y, v, psi, r):
 		self.m = 100
@@ -15,15 +16,20 @@ class Car():
 		self.Cr = 1
 		self.Lf = 2
 		self.Lr = 2
-		self.Iz = 10
-		self.u0 = 1
+		self.Iz = 100
+		self.u0 = 2
 
 		self.y = y
 		self.v = v
 		self.psi = psi
 		self.r = r
 
-	def update(self, delta):
+		self.t = 0
+
+		self.cx = 0
+		self.cy = 0
+
+	def update(self, delta, dt=1):
 		A = np.zeros((4,4))
 		B = np.zeros((4,1))
 		X = np.zeros((4,1))
@@ -47,7 +53,7 @@ class Car():
 
 		U[0,0] = delta
 
-		result = np.matmul(A,X)+np.matmul(B,U)
+		result = dt*np.matmul(A,X)+np.matmul(B,U)
 
 		self.y += result[0,0]
 		self.v += result[1,0]
@@ -58,19 +64,48 @@ class Car():
 		X[1,0] = self.v
 		X[2,0] = self.psi
 		X[3,0] = self.r
-		print(X)
 
-def main():
-	print("Starting")
+		rotMatrix = np.array([[math.cos(self.psi),-1*math.sin(self.psi)],[math.sin(self.psi),math.cos(self.psi)]])
+		movementVector = np.matmul(np.array([self.u0,self.v]).T,rotMatrix)
+		positionVector = np.array([self.cx,self.cy]).T + movementVector
+		self.cx = positionVector[0]
+		self.cy = positionVector[1]
 
-	#fig = plt.figure()
-	#ax = plt.subplot()
+		print(positionVector)
 
-	car = Car(-50,1,0,0)
-	while True:
-		car.update(0)
-		input()
+	def plot(self,ax):
+		ax.scatter(self.cx, self.cy,100,'k',zorder=1)
+
+def animate(t):
+	car.update(0.1*math.sin(t*0.01)) # Steering
+	xdata.append(car.cx)
+	ydata.append(car.cy)
+	particle.set_data(car.cx,car.cy)
+	path.set_data(xdata,ydata)
+	return particle,path
+
+car = Car(0,0,0,0)
+
+fig = plt.figure()
+ax = plt.subplot()
+
+xdata,ydata = [],[]
+particle, = ax.plot([], [],'ko')
+path, = ax.plot([],[],'b--',zorder=1)
+ax.set_xlim([-1000,1000])
+ax.set_ylim([-1000,1000])
+ax.set_aspect('equal')
+ax.grid()
+plt.tight_layout()
 
 
-if __name__ == '__main__':
-	main()
+anim = animation.FuncAnimation(fig, animate,
+	#frames = 10,
+	interval = 10,
+	blit = True)
+
+plt.show()
+
+
+
+
